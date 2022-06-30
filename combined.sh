4d94f73d1e7b731bcea4ad23b720767e4e244d1a
Alejandro Delgado <36162270+adelgadorios@users.noreply.github.com>
2022-06-30 16:22:09 -0400

    Create combined.sh

---
+++ combined.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash -l
+#SBATCH -o std_out3
+#SBATCH -e std_err3
+#SBATCH -p Quick 
+#SBATCH --gpus=8  
+conda activate translator
+./run_baseline_system.sh gn ../../helsinki+uruguay+constitution translations/helsinki+uruguay+constitution-results 100 -CLI
