e56d0883fe18ca0bb1c4ff465ea4686aeaa0e476
Alejandro Delgado <36162270+adelgadorios@users.noreply.github.com>
2022-06-30 16:19:36 -0400

    Update and rename run_job.sh to original.sh

--- americasnlp2021/baseline_system/run_job.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -l
-#SBATCH -o std_out
-#SBATCH -e std_err
-#SBATCH -p Quick 
-#SBATCH --cpus-per-task=32 
-#SBATCH --mem=100GB 
-#SBATCH --gpus=8  
-conda activate translator
-./run_baseline_system.sh gn ../data/guarani-spanish augmented-data-set 200 -CLI
