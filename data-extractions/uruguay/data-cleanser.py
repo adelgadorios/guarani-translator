4e14eeca88df4836c98b7db09030488e4f3f780b
Alejandro <delgadoale00@protonmail.com>
2022-07-07 08:42:49 -0400

    update

--- data-extractions/uruguay/data-cleanser.py
+++ data-extractions/uruguay/data-cleanser.py
@@ -1,3 +1,13 @@
+"""
+        #scrapped for now as I need to ook at the possible effects this may have on accuracy
+            line_gn = line_gn.replace('î', 'ĩ')
+            line_gn = line_gn.replace('ä', 'ĩ')
+            line_gn = line_gn.replace('ÿ', 'ỹ')
+            line_gn = line_gn.replace('ê', 'ẽ')
+            line_gn = line_gn.replace('â', 'ã')
+            line_gn = line_gn.replace('ô', 'õ')
+            line_gn = line_gn.replace('û', 'ũ')
+""" 
 def find_common(s0, s1):
     s0 = s0.lower()
     s1 = s1.lower()
@@ -15,16 +25,6 @@ with open("news-data.txt", "r") as f1, open("train-uruguay.es", "w") as te, open
             line_gn = line_gn.replace('“', '"')
             line_gn = line_gn.replace('’', "'")
             line_gn = line_gn.replace('”', '"')
-           """
-           #scrapped for now as I need to ook at the possible effects this may have on accuracy
-            line_gn = line_gn.replace('î', 'ĩ')
-            line_gn = line_gn.replace('ä', 'ĩ')
-            line_gn = line_gn.replace('ÿ', 'ỹ')
-            line_gn = line_gn.replace('ê', 'ẽ')
-            line_gn = line_gn.replace('â', 'ã')
-            line_gn = line_gn.replace('ô', 'õ')
-            line_gn = line_gn.replace('û', 'ũ')
-           """ 
             line_gn = " ".join(line_gn.split())
             line_es = next(f1)
             line_es = " ".join(line_es.split())
@@ -33,7 +33,7 @@ with open("news-data.txt", "r") as f1, open("train-uruguay.es", "w") as te, open
             line_es = line_es.replace('\n', '')
             line_es = line_es.replace("es:", '')
             common = find_common(line_gn, line_es)
-            if line_es == "Pág" or len(line_es) > 2*len(line_gn) or len(line_gn) < 40 or common > 4 or len(line_gn) > len(line_es):
+            if line_es == "Pág" or len(line_es) > 2*len(line_gn) or len(line_gn) < 40 or common > 8 or len(line_gn) > len(line_es):
                 continue
             te.write(line_es + '\n')
             tg.write(line_gn + '\n')
